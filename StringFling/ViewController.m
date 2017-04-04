001b6a31f132071c705dfb7de70d17669755979c
Shin Park <shinpark@shins-mac-mini.home>
2017-04-03 20:57:08 -0500

    Objective-C Strings with NSString

--- StringFling/ViewController.m
+++ StringFling/ViewController.m
@@ -17,6 +17,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *firstName = @"John";
+    NSString *allocatedString = [[NSString alloc]init];
+    
+    NSLog(@"Name: %@", firstName);
+    
+    NSString *fullName = [NSString stringWithFormat:@"%@ Smith %@", firstName, @"Charles the III"];
+    
+    NSString *display = [fullName stringByAppendingString:@" - Died 1448"];
+    
+    NSLog(@"Fullname: %@", fullName);
+    
+    NSLog(@"%@", display);
+    
+    NSString *var1 = @"Junk";
+    NSString *var2 = @"in the trunk";
+    NSString *var3 = @"junk";
+    
+    if (![var1 isEqualToString:var2]) {
+        NSLog(@"We should see this!");
+    }
+    
+    if ([var1.lowercaseString isEqualToString:var3]) {
+        NSLog(@"We gotteth here!");
+    }
+    
+    if ([var1 caseInsensitiveCompare:var3] == NSOrderedSame) {
+        NSLog(@"Woohoo!");
+    }    
+    
 }
 
 
